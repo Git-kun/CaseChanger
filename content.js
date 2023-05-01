5dee790bff97387357bf07052d5b02272f4da3c1
Git-kun <56929408+Git-kun@users.noreply.github.com>
2023-05-01 23:10:05 +0900

    fix

--- content.js
+++ content.js
@@ -1,6 +1,12 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.action == "getSelection") {
-    var selectedText = window.getSelection().toString();
-    sendResponse({text: selectedText});
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  // let title = document.querySelector('.style-bw4dop').textContent
+
+  // 選択されたテキストを取得
+
+  let title = window.getSelection().toString();
+
+  // 取得したテキストが空でない場合、sendResponseで返す
+  if (title !== '') {
+    sendResponse(title);
   }
 });
--- popup.js
+++ popup.js
@@ -1,11 +1,62 @@
-chrome.commands.onCommand.addListener(function(command) {
-  if (command == "convert_to_camel_case") {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {action: "getSelection"}, function(response) {
-        var selectedText = response.text;
-        var convertedText = convertToCamelCase(selectedText);
-        document.getElementById("output").textContent = convertedText;
-      });
+// ポップアップが開かれたときに実行される処理
+document.addEventListener("DOMContentLoaded", function() {
+  // Get Selected Textボタンをクリックしたときの処理
+// document.getElementById('btn_get').addEventListener('click', () => {
+    chrome.tabs.query( {active:true, currentWindow:true}, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, {message: 'getname'}, (content) => {
+            if(!content){
+                alert('Cannot Get! Try Reload First!');
+                return;
+            }
+            // var content = content.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+            var str0 = content.replace(/\s+/g, "-");
+            var str1 = content.replace(/\s+/g, "_");
+            var str2 = content.replace(/\s+/g, "");
+            var str3 = content.toUpperCase();
+            var str4 = content.toLowerCase();
+            // 処理後の値をdatで表示する
+            document.getElementById("dat0").value = str0;
+            document.getElementById("dat1").value = str1;
+            document.getElementById("dat2").value = str2;
+            document.getElementById("dat3").value = str3;
+            document.getElementById("dat4").value = str4;
+            document.getElementById('title').value = content
+            
+        });
+        
     });
-  }
+    
+// });
+});
+
+
+// popup.js
+
+// ページがロードされたときに実行される
+window.addEventListener('load', function() {
+  // title要素を取得する
+  var titleElement = document.getElementById('title');
+  // output要素を取得する
+  var outputElement = document.getElementById('output');
+
+  // title要素の値が変更されたときに実行される関数を定義する
+  titleElement.addEventListener('input', function() {
+    // 入力されたテキストを取得する
+    var text = titleElement.value;
+
+    // キャメルケースに変換する
+    var camelCaseText = text.replace(/-([a-z])/g, function(match, p1) {
+      return p1.toUpperCase();
+    });
+
+    // ケバブケースに変換する
+    var kebabCaseText = text.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+
+    // 変換されたテキストを表示する
+    console.log('キャメルケース:', camelCaseText);
+    console.log('ケバブケース:', kebabCaseText);
+
+    // output要素に変換されたテキストを表示する
+    outputElement.textContent = `キャメルケース: ${camelCaseText}, ケバブケース: ${kebabCaseText}`;
+  });
 });
