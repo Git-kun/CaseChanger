2fb87837092b10254db85acaa04e22445827357e
柏正隆 <o60.kashiwa.masataka@gmail.com>
2024-04-05 01:36:55 +0900

    コメントアウト削除

--- popup.js
+++ popup.js
@@ -2,9 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
       chrome.tabs.sendMessage(tabs[0].id, {action: 'getSelectedText'}, function(response) {
           if (response && response.text) {
-              updateTextFields(response.text); // 最初のテキスト変換とフィールド更新
-          } else {
-              document.getElementById('convertedText').textContent = "テキストが選択されていません";
+              updateTextFields(response.text);
           }
       });
   });
@@ -14,10 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   function updateTextFields(text) {
-    // 先頭と末尾のスペースを削除
     const trimmedText = text.trim();
-
-    // 変換されたテキストを各フィールドにセット
     document.getElementById('inputText').value = trimmedText;
     document.getElementById("kebabCase").value = toKebabCase(trimmedText);
     document.getElementById("snakeCase").value = toSnakeCase(trimmedText);
@@ -31,26 +26,18 @@ document.addEventListener("DOMContentLoaded", function() {
 
   function toKebabCase(text) {
     return text
-        // 最初にキャメルケースの単語境界をハイフンで分割
         .replace(/([a-z])([A-Z])/g, '$1-$2')
-        // アンダースコアとスペースをハイフンに置換
         .replace(/[_\s]+/g, '-')
-        // 全て小文字に変換
         .toLowerCase();
   }
 
 
   function toSnakeCase(text) {
     return text
-        // スペース、ハイフン、アンダースコアをアンダースコアに統一
         .replace(/[\s\-]+/g, '_')
-        // 大文字の前にアンダースコアを挿入し、キャメルケースを適切に処理
         .replace(/([A-Z])/g, '_$1')
-        // 連続するアンダースコアを一つにする
         .replace(/_+/g, '_')
-        // 先頭がアンダースコアの場合は削除
         .replace(/^_/, '')
-        // 全て小文字に変換
         .toLowerCase();
   }
 
@@ -68,9 +55,7 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   function toCamelCase(text) {
-      // キャメルケース形式のテキストを適切に処理するための追加のチェック
       if (!/[\s-_]/.test(text) && /[A-Z]/.test(text)) {
-          // キャメルケース形式のテキストの単語境界を識別
           return text
               .replace(/\.?([A-Z]+)/g, function (x,y){ return " " + y.toLowerCase(); })
               .trim()
@@ -78,7 +63,6 @@ document.addEventListener("DOMContentLoaded", function() {
               .map((word, index) => index === 0 ? word.toLowerCase() : word.charAt(0).toUpperCase() + word.slice(1))
               .join('');
       } else {
-          // 通常のスペース、ハイフン、アンダースコアで区切られているテキストの処理
           return text
               .split(/[\s-_]+/)
               .map((word, index) => index === 0 ? word.toLowerCase() : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
@@ -87,14 +71,10 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   function toPascalCase(text) {
-    // まず、キャメルケースの単語境界を識別しやすくするために、
-    // 大文字の前にスペースを挿入します（ただし、文字列の先頭は除く）。
-    // そして、アンダースコアやハイフンもスペースに置換します。
     const spacedText = text
         .replace(/([a-z])([A-Z])/g, '$1 $2')
         .replace(/[-_]+/g, ' ');
 
-    // スペースで区切られたテキストをパスカルケースに変換
     return spacedText
         .split(' ')
         .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
