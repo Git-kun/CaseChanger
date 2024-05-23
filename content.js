e482019c68db5891e0c23ef40b34242f221727b0
柏正隆 <o60.kashiwa.masataka@gmail.com>
2024-05-23 23:28:36 +0900

    fix

--- content.js
+++ content.js
@@ -1,11 +1,4 @@
-chrome.runtime.onMessage.addListener(function(request, sendResponse) {
-  if (request.action === "convertText") {
-    const selectedText = window.getSelection().toString();
-    if (selectedText) {
-      sendResponse({text: convertedText});
-    }
-  }
-
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
   if (request.action === 'getSelectedText') {
     const selectedText = window.getSelection().toString();
