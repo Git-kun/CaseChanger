2ef9d067564d89aae427d51adb0a79257544c540
Git-kun <56929408+Git-kun@users.noreply.github.com>
2024-06-15 22:32:48 +0900

    fix

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'getSelectedText') {
     const selectedText = window.getSelection().toString();
     sendResponse({ text: selectedText });
   }
 });
+
